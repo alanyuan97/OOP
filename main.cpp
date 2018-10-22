c5fe71eac83f62150251c8a55aa22c662bd45bf1
alanyuan97 <alanyuan97@gmail.com>
2018-10-22 22:41:28 +0100

    Lab exercise with Triangle

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include "point.hpp"
+#include "triangle.hpp"
 using namespace std;
 
 int main (){
   cout<<"##PART 1 -----------------------------"<<endl;
-  point p1(0,0),p2(1,2),p3(0.0,0.0);
+  point p1(0,0),p2(1,2),p3(4,5);
   cout<<"(p1==p2):  "<<(p1==p2)<<endl;
-  
+
+  cout<<"#PART 2 --------------------------------"<<endl;
+  triangle t1(p1,p2,p3);
+  t1.update_perimeter();
+  t1.display();
 
   return 0;
 }
