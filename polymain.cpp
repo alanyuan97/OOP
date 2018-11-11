21c82c86b008ec12099aedc177c2e1a1724d72ad
alanyuan97 <36449601+alanyuan97@users.noreply.github.com>
2018-11-11 23:57:04 +0000

    operator []

--- polymain.cpp
+++ polymain.cpp
@@ -11,28 +11,29 @@ int main (){
   cout<<p<<endl;
   std::cerr << "******************* TEST TWO *********************" << '\n';
   polynomial p1(p);
-  cout<<"default copy constructor => p1:\n"<<p1<<endl;
+  p.at(0)=1;
+  p.at(1)=2;
+  p.at(2)=4;
+  cout<<"user-defined copy constructor => p:\n"<<p<<endl;
+  cout<<"user-defined copy constructor => p1:\n"<<p1<<endl;
   std::cerr << "******************* TEST THREE *********************" << '\n';
   polynomial p2=p;
+  p.at(0)=2;
+  p.at(1)=3;
+  p.at(2)=1;
+  cout<<"default assign => p:\n"<<p<<endl;
   cout<<"default assign => p2:\n"<<p2<<endl;
+
   std::cerr << "******************* TEST FOUR *********************" << '\n';
   p=p;
-  polynomial p3=p;
-  cout<<"self copy => p3:\n"<<p3<<endl;
+  cout<<"self copy => p:\n"<<p<<endl;
   p.at(0)=1;
   p.at(1)=2;
   p.at(2)=4;
   cout<<"P:\n"<<p<<endl;
-  cout<<"p3:\n"<<p3<<endl;
   std::cerr << "******************* TEST FOUR *********************" << '\n';
   polynomial p4=p2=p1;
   cout<<"chain assign => p4:\n"<<p4<<endl;
-  int a, b, c;
-  a = b = c = 1;
-
-  cout<<a << b << c <<endl;
-  int arr[3];
-  cout << arr << endl;
 
   return 0;
 }
